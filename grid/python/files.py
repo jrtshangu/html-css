e444555b9d591b5fc8482eb539e9ac95255c6f91
jrtshangu <66824480+jrtshangu@users.noreply.github.com>
2023-12-01 14:32:01 +0100

    creating python file

---
+++ grid/python/files.py
@@ -0,0 +1,12 @@
+#practicing files csv file type
+with ("My-files.csv", "a") as file_cvs:
+    name= input("What is your first name? >: ")
+    last_name= input("What is your last name? >: ")
+    Phone_number= int(input("What is your phone number? >: "))
+
+    file_cvs.write(name)
+    file_cvs.write("\t")
+    file_cvs.write(last_name)
+    file_cvs.write("\t")
+    file_cvs.write(Phone_number)
+    print(file_cvs)
